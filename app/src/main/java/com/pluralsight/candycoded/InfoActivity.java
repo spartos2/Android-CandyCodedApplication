ff4b6f58b941548828d51c0daba0dde968a2fe0f
spartos2 <benjaminspartos@gmail.com>
2020-09-28 19:22:52 +0300

    create action_next intent, startActivity

--- app/src/main/java/com/pluralsight/candycoded/InfoActivity.java
+++ app/src/main/java/com/pluralsight/candycoded/InfoActivity.java
@@ -34,7 +34,9 @@ public class InfoActivity extends AppCompatActivity {
     }
 
     public void createPhoneIntent(View view) {
-        
+        Intent phoneIntent = new Intent(Intent.ACTION_DIAL);
+        phoneIntent.setData(Uri.parse("tel:0123456789"));
+        startActivity(phoneIntent);
     }
 
     // ***
